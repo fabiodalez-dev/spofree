ab57e6ac7381e22cb49fea1c84608b22b846dcf9
pidi <peterdbaltes@gmail.com>
2025-12-09 21:09:18 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -3,18 +3,17 @@ import { Track } from './types';
 
 // List of public Hifi API instances provided by the user.
 export const API_INSTANCES = [
+  'https://frankfurt.monochrome.tf',
+  'https://triton.squid.wtf', 
   'https://ohio.monochrome.tf',
   'https://virginia.monochrome.tf',
   'https://oregon.monochrome.tf',
-  'https://frankfurt.monochrome.tf',
   'https://singapore.monochrome.tf',
-  'https://triton.squid.wtf',
   'https://wolf.qqdl.site',
   'https://maus.qqdl.site',
   'https://vogel.qqdl.site',
   'https://katze.qqdl.site',
   'https://hund.qqdl.site',
-  'https://tidal.401658.xyz'
 ];
 
 // Select the first instance by default
