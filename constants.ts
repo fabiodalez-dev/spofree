6634c45f1e90e30f1a32b9522b181ef5651d544d
pidi <peterdbaltes@gmail.com>
2025-12-15 12:53:38 +0100

    removed .monochrome.tf domains because theyre dead

--- constants.ts
+++ constants.ts
@@ -3,12 +3,6 @@ import { Track } from './types';
 
 // List of public Hifi API instances provided by the user.
 export const API_INSTANCES = [
-  'https://frankfurt.monochrome.tf',
-  'https://triton.squid.wtf', 
-  'https://ohio.monochrome.tf',
-  'https://virginia.monochrome.tf',
-  'https://oregon.monochrome.tf',
-  'https://singapore.monochrome.tf',
   'https://wolf.qqdl.site',
   'https://maus.qqdl.site',
   'https://vogel.qqdl.site',
